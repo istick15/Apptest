f4656ce32a1cf48218b3000995dcd05c3d4a9b10
Siriwat Suttipanyo <jusmanz1015@gmail.com>
2020-02-24 00:24:28 +0700

    add mapbox

--- App.js
+++ App.js
@@ -7,12 +7,12 @@
  */
 
 import React from 'react';
-import {Text} from 'react-native';
+import MapTour from './screens/MapTour';
 
 const App = () => {
   return (
     <>
-      <Text>Test</Text>
+      <MapTour />
     </>
   );
 };
